e7c5cef1f374d34ddfe5412ba8cb883315146aad
antonmalykhin <a.y.malykhin@gmail.com>
2020-01-27 15:48:12 +0300

    Добавляет папку js

---
+++ source/js/script.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var menuItems = document.querySelectorAll('.page-header__main-menu .main-menu__link');
+console.log(menuItems);
+
+menuItems.forEach(i, function (i) {
+  console.log(menuItems[i]);
+})
